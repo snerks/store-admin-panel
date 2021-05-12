c5bf17cbc95cd5b0910bdd04e7d407c1c65ae1f6
snerks <martin.github@outlook.com>
2021-05-12 16:55:35 +0100

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-const routes: Routes = [];
+import { DashComponent } from "./dash/dash.component";
+
+const routes: Routes = [{ path: "dashboard", component: DashComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
